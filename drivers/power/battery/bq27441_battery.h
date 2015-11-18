f5c805d9ef9666027e6eea685ce5d50181bf087a
liuwei <liuwei@actions-semi.com>
2015-11-18 12:50:27 +0800

    power: add bq27441_battery driver
    Change-Id: I32d00b35af005240e4e663fb085f5680901e06bc

---
+++ drivers/power/battery/bq27441_battery.h
@@ -0,0 +1,14 @@
+/*
+* TI BQ27441 battery driver
+*
+ * Copyright (c) 2015 Actions Semiconductor Co., Ltd.
+ * Terry Chen chenbo@actions-semi.com
+*
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __BQ27441_BATTERY_H__
+#define __BQ27441_BATTERY_H__
+
+int bq27441_battery_init(const void *blob);
+#endif
+
